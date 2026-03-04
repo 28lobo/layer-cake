4e414409f6ad5b4d74b3015b091a116219ee05c7
28lobo <lobo.carroll@gmail.com>
2026-03-04 14:54:40 -0800

    update

--- app/(frontend)/page.tsx
+++ app/(frontend)/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link'
 import { Title } from '@/components/title'
+import Button from '@/components/button'
 
 export default async function Page() {
   return (
     <section className="container mx-auto grid grid-cols-1 gap-6 p-12">
       <Title>Layer Caker Home Page</Title>
+      <Button>
+        {children}
+      </Button>
       <hr />
       <Link href="/posts">Posts index &rarr;</Link>
     </section>
